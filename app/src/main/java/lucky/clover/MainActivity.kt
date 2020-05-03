c3ca75c9d2f39326be67f5337e1f109062963be0
MartenCatcher <kpbica40k@gmail.com>
2020-05-03 08:13:44 +0300

    Add google map service check

--- app/src/main/java/lucky/clover/MainActivity.kt
+++ app/src/main/java/lucky/clover/MainActivity.kt
@@ -1,18 +1,34 @@
 package lucky.clover
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.GoogleApiAvailability
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private const val TAG: String = "MainActivity"
+        private const val ERROR_DIALOG_REQUEST = 9001
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (checkMapService()) {
+            init()
+        }
+    }
+
+    private fun init() {
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
         val navController = findNavController(R.id.nav_host_fragment)
@@ -25,5 +41,27 @@ class MainActivity : AppCompatActivity() {
         ))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+
+    private fun checkMapService(): Boolean {
+        Log.d(TAG, "checkMapService: starting...")
+
+        val status: Int = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(this)
+
+        when {
+            status == ConnectionResult.SUCCESS -> {
+                Log.d(TAG, "checkMapService: the service is available")
+                return true
+            }
+            GoogleApiAvailability.getInstance().isUserResolvableError(status) -> {
+                Log.d(TAG, "checkMapService: trying to resolve the issue $status")
+                val dialog = GoogleApiAvailability.getInstance().getErrorDialog(this, status, ERROR_DIALOG_REQUEST)
+                dialog.show()
+            }
+            else -> {
+                Toast.makeText(this, "Can't make map request", Toast.LENGTH_SHORT).show()
+            }
+        }
+        return false
     }
 }

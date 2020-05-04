b86a324d5c8bc88193029f4e0b1e1b4ccb30af97
MartenCatcher <kpbica40k@gmail.com>
2020-05-04 17:24:52 +0300

    Enable location service

--- app/src/main/java/lucky/clover/ui/map/MapFragment.kt
+++ app/src/main/java/lucky/clover/ui/map/MapFragment.kt
@@ -2,6 +2,7 @@ package lucky.clover.ui.map
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -10,6 +11,8 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.*
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
@@ -22,18 +25,20 @@ class MapFragment : Fragment(), OnMapReadyCallback {
         private const val LOCATION_PERMISSION_RESULT_CODE = 8001
         private const val FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION
         private const val COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION
+        private const val ZOOM = 15f
     }
 
-    private var mPermissionGranted: Boolean = false
+    private var mLocationPermissionGranted: Boolean = false
     private lateinit var mMapView: MapView
     private lateinit var mMap: GoogleMap
+    private lateinit var mFusedLocationProviderClient: FusedLocationProviderClient
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        Log.d(TAG, "Map: instantiating...")
+        Log.d(TAG, "onCreateView: instantiating...")
         val root = inflater.inflate(R.layout.fragment_map, container, false)
 
         mMapView = root.findViewById(R.id.text_map)
@@ -42,7 +47,7 @@ class MapFragment : Fragment(), OnMapReadyCallback {
 
         getLocationPermission()
 
-        if (mPermissionGranted) {
+        if (mLocationPermissionGranted) {
             initMap()
         }
 
@@ -64,7 +69,7 @@ class MapFragment : Fragment(), OnMapReadyCallback {
 
     private fun getLocationPermission() {
         if (isPermissionGranted(FINE_LOCATION) && isPermissionGranted(COARSE_LOCATION)) {
-            mPermissionGranted = true
+            mLocationPermissionGranted = true
         } else {
             Log.d(TAG, "Map: permissions called")
             requestPermissions(
@@ -79,20 +84,19 @@ class MapFragment : Fragment(), OnMapReadyCallback {
         permissions: Array<out String>,
         grantResults: IntArray
     ) {
-        Log.d(TAG, "Map: got permission $requestCode")
         if (requestCode == LOCATION_PERMISSION_RESULT_CODE) {
-            mPermissionGranted = false
+            mLocationPermissionGranted = false
             if (grantResults.isNotEmpty()) {
                 for (grantResult in grantResults) {
                     if (grantResult != PackageManager.PERMISSION_GRANTED) {
-                        mPermissionGranted = false
-                        Log.d(TAG, "Map: revoked")
+                        mLocationPermissionGranted = false
+                        Log.d(TAG, "onRequestPermissionsResult: permissions revoked")
                         return
                     }
                 }
 
-                Log.d(TAG, "Map: granted")
-                mPermissionGranted = true
+                Log.d(TAG, "onRequestPermissionsResult: permissions granted")
+                mLocationPermissionGranted = true
                 initMap()
             }
         }
@@ -123,9 +127,14 @@ class MapFragment : Fragment(), OnMapReadyCallback {
         Log.d(TAG, "Map: ready")
         mMap = googleMap
 
-        // For showing a move to my location button
         mMap.isMyLocationEnabled = true
 
+        if (mLocationPermissionGranted) {
+            getDeviceLocation()
+            mMap.isMyLocationEnabled = true
+            mMap.uiSettings.isMyLocationButtonEnabled = false
+        }
+        
         // For dropping a marker at a point on the Map
         val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(
@@ -136,5 +145,41 @@ class MapFragment : Fragment(), OnMapReadyCallback {
         val cameraPosition =
             CameraPosition.Builder().target(sydney).zoom(12f).build()
         mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
+    }
+
+    private fun getDeviceLocation() {
+        Log.d(TAG, "getDeviceLocation: getting location...")
+
+        mFusedLocationProviderClient =
+            LocationServices.getFusedLocationProviderClient(this.requireContext())
+
+        try {
+            if (mLocationPermissionGranted) {
+                val lastLocation = mFusedLocationProviderClient.lastLocation
+                lastLocation.addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Log.d(TAG, "getDeviceLocation: current location is set")
+                        val location: Location? = task.result
+                        location?.let { it -> LatLng(it.latitude, it.longitude) }?.let { it -> moveCamera(it) }
+                    } else {
+                        Log.d(TAG, "getDeviceLocation: current location is unknown")
+                        Toast.makeText(
+                            this.requireContext(),
+                            "Unable to get current location",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+            } else {
+                Log.d(TAG, "getDeviceLocation: permissions aren't granted")
+            }
+        } catch (e: SecurityException) {
+            Log.d(TAG, "getDeviceLocation: SecurityException $e")
+        }
+    }
+
+    private fun moveCamera(latLng: LatLng) {
+        Log.d(TAG, "moveCamera: coordinates $latLng")
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, ZOOM))
     }
 }

6b97a7df9bb2d18a7834cf38f2a74a3ec33d8d7a
MartenCatcher <kpbica40k@gmail.com>
2020-05-04 11:47:55 +0300

    Fix permission request

--- app/src/main/java/lucky/clover/ui/map/MapFragment.kt
+++ app/src/main/java/lucky/clover/ui/map/MapFragment.kt
@@ -7,6 +7,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
@@ -72,8 +73,7 @@ class MapFragment : Fragment(), OnMapReadyCallback {
             mPermissionGranted = true
         } else {
             Log.d(TAG, "Map: permissions called")
-            ActivityCompat.requestPermissions(
-                this.requireActivity(),
+            requestPermissions(
                 arrayOf(FINE_LOCATION, COARSE_LOCATION),
                 LOCATION_PERMISSION_RESULT_CODE
             )
@@ -125,6 +125,7 @@ class MapFragment : Fragment(), OnMapReadyCallback {
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
+        Toast.makeText(this.requireContext(), "Map is ready", Toast.LENGTH_SHORT).show()
         Log.d(TAG, "Map: ready")
         mMap = googleMap
 
